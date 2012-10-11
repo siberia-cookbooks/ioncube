1dffa4728224eebd175bf9738197f17c6fcf62e3
Jacques Marneweck <jacques@powertrip.co.za>
2012-10-11 20:31:15 +0000

    Untar just the two loaders and install them
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,3 +10,12 @@
 remote_file "/var/tmp/ioncube_loaders_sun_x86.tar.bz2" do
   source "http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_sun_x86.tar.bz2"
 end
+
+execute "untar-ioncube-loaders" do
+  command "gtar -jvxf ioncube_loaders_sun_x86.tar.bz2 ioncube/ioncube_loader_sun_5.4.so ioncube/ioncube_loader_sun_5.4_ts.so"
+  cwd "/var/tmp"
+end
+
+execute "copy-ioncube-loaders" do
+  command "cp -pr /var/tmp/ioncube/ioncube_loader_sun_5.4{,_ts}.so /opt/local/lib/php/20120301"
+end
